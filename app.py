cbcb5787ad044c1c24b9d73b41e89b03cf7b758d
Jackie <repojackie@gmail.com>
2020-08-24 21:44:17 -0400

    made some useless changes

--- app.py
+++ app.py
@@ -3,11 +3,24 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bagels123are123delicious'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlitte://test.db'
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
-@app.route('/')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index_login():
+    if not session.get('logged_in'):
+        return render_template('login.html')
     return render_template('login.html')
 
 @app.route('/mainpage')
